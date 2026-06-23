fdf2e06263602735967dcd85c16dc127a27f422a
Hana-Suyama <115055069+Hana-Suyama@users.noreply.github.com>
2026-06-23 15:19:11 +0900

    BoxFilter

--- project/2025_CG2_DirectX/engine/DirectXBasic.cpp
+++ project/2025_CG2_DirectX/engine/DirectXBasic.cpp
@@ -342,11 +342,11 @@ void DirectXBasic::CreateRenderTexturePSO()
 	depthStencilDesc.DepthEnable = false;
 
 	//Shaderをコンパイルする
-	Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob = CompileShader(L"resources/shaders/Vignette.VS.hlsl",
+	Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob = CompileShader(L"resources/shaders/BoxFilter5x5.VS.hlsl",
 		L"vs_6_0", logger_);
 	assert(vertexShaderBlob != nullptr);
 
-	Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob = CompileShader(L"resources/shaders/Vignette.PS.hlsl",
+	Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob = CompileShader(L"resources/shaders/BoxFilter5x5.PS.hlsl",
 		L"ps_6_0", logger_);
 	assert(pixelShaderBlob != nullptr);
 
