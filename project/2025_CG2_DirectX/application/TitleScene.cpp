2493b1b7fdaac3043ad409a411a58fa43fad8473
Hana-Suyama <115055069+Hana-Suyama@users.noreply.github.com>
2026-08-14 15:10:53 +0900

    ライトを修正

--- project/2025_CG2_DirectX/application/TitleScene.cpp
+++ project/2025_CG2_DirectX/application/TitleScene.cpp
@@ -1,14 +1,44 @@
 #include "TitleScene.h"
 
+#include "../engine/Light/DirectionalLight.h"
+#include "../engine/Light/PointLight.h"
+#include "../engine/Light/SpotLight.h"
 #include "../engine/Scene/SceneManager.h"
 #include "../engine/WindowsApi.h"
 #include "GameScene.h"
 
 void TitleScene::Initialize(
-	DirectXBasic*, Object3DBasic*, SkinnedObject3DBasic*, ModelManager*, Logger*,
+	DirectXBasic* directXBasic, Object3DBasic* object3dBasic,
+	SkinnedObject3DBasic*, ModelManager*, Logger*,
 	SRVManager*, TextureManager* textureManager, SpriteBasic* spriteBasic,
 	XAudio2Basic*, std::mt19937*)
 {
+	directXBasic_ = directXBasic;
+
+	camera_ = std::make_unique<Camera>();
+	camera_->SetRotate({ 0.0f, 0.0f, 0.0f });
+	camera_->SetTranslate({ 0.0f, 0.0f, -10.0f });
+	camera_->Update();
+	object3dBasic->SetDefaultCamera(camera_.get());
+
+	cameraForGPUResource_ =
+		directXBasic_->CreateBufferResource(sizeof(CameraForGPU));
+	cameraForGPUResource_->Map(
+		0, nullptr, reinterpret_cast<void**>(&cameraForGPUData_));
+	cameraForGPUData_->worldPosition = camera_->GetTranslate();
+
+	lights_.push_back(std::make_unique<DirectionalLight>());
+	lights_.back()->Initialize();
+	lights_.push_back(std::make_unique<PointLight>());
+	lights_.back()->Initialize();
+	lights_.push_back(std::make_unique<SpotLight>());
+	lights_.back()->Initialize();
+
+	lightsBufferResource_ =
+		directXBasic_->CreateBufferResource(sizeof(LightBuffer));
+	lightsBufferResource_->Map(
+		0, nullptr, reinterpret_cast<void**>(&lightsBufferData_));
+
 	const std::string titleLogoTexturePath = "resources/TitleLogo.png";
 	textureManager->LoadTexture(titleLogoTexturePath);
 
@@ -31,11 +61,32 @@ void TitleScene::Update()
 		sceneManager_->SetNextScene(std::move(scene));
 	}
 
+	camera_->Update();
+	cameraForGPUData_->worldPosition = camera_->GetTranslate();
 	titleLogoSprite_->Update();
 }
 
 void TitleScene::SpriteDraw()
 {
+	std::vector<LightData> lightDataVector;
+	lightDataVector.reserve(lights_.size());
+	for (auto& light : lights_) {
+		LightData data{};
+		light->WriteToLightData(data);
+		lightDataVector.push_back(data);
+	}
+
+	lightsBufferData_->lightCount = static_cast<uint32_t>(lights_.size());
+	memcpy(
+		lightsBufferData_->lights,
+		lightDataVector.data(),
+		sizeof(LightData) * lightsBufferData_->lightCount);
+
+	directXBasic_->GetCommandList()->SetGraphicsRootConstantBufferView(
+		3, lightsBufferResource_->GetGPUVirtualAddress());
+	directXBasic_->GetCommandList()->SetGraphicsRootConstantBufferView(
+		4, cameraForGPUResource_->GetGPUVirtualAddress());
+
 	titleLogoSprite_->Draw();
 }
 
--- project/2025_CG2_DirectX/application/TitleScene.h
+++ project/2025_CG2_DirectX/application/TitleScene.h
@@ -19,5 +19,15 @@ public:
 	void Finalize() override;
 
 private:
+	DirectXBasic* directXBasic_ = nullptr;
+	std::unique_ptr<Camera> camera_;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> cameraForGPUResource_;
+	CameraForGPU* cameraForGPUData_ = nullptr;
+
+	std::vector<std::unique_ptr<Light>> lights_;
+	Microsoft::WRL::ComPtr<ID3D12Resource> lightsBufferResource_;
+	LightBuffer* lightsBufferData_ = nullptr;
+
 	std::unique_ptr<Sprite> titleLogoSprite_;
 };
